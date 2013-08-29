dcb4668cc212c6a496400cb44433ec1131c9e361
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-08-29 13:11:09 -0600

    Get ui-router states

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,49 @@
 'use strict';
 
-angular.module('geboClientApp', ['ngResource'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/token', {
-        templateUrl: 'views/token.html',
-        controller: 'MainCtrl'
-      })
-      .when('/app', {
-        templateUrl: 'views/app.html',
-        controller: 'AppCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('geboClientApp', ['ngResource', 'ui.bootstrap', 'ui.compat'])
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        // For an unmatched URL
+        $urlRouterProvider.otherwise('/');
+
+        // States
+        $stateProvider.
+            state('home', {
+                url: '/',
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            }).
+            state('token', {
+                url: '/token',
+                templateUrl: 'views/token.html',
+                controller: 'MainCtrl'
+            }).
+            state('app', {
+                url: '/app',
+                templateUrl: 'views/app.html',
+                controller: 'AppCtrl'
+            }).
+            state('app.lists', {
+                templateUrl: 'views/app.lists.html'
+            });
+
+     });
+//  .config(function ($routeProvider) {
+//    $routeProvider
+//      .when('/', {
+//        templateUrl: 'views/main.html',
+//        controller: 'MainCtrl'
+//      })
+//      .when('/token', {
+//        templateUrl: 'views/token.html',
+//        controller: 'MainCtrl'
+//      })
+//      .when('/app', {
+//        templateUrl: 'views/app.html',
+//        controller: 'AppCtrl'
+//      })
+//      .otherwise({
+//        redirectTo: '/'
+//      });
+//  });
 
